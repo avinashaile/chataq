2e60f637049a8b4d081f90fda2cc052b303c9020
avinashaile <avinash.ily88@gmail.com>
2023-05-03 23:45:54 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Component } from "react"
+import Props from "./Props"
+
+class App extends Component{
+    render() {
+    return(
+        <Props data="Hai" data1="Environment"/>
+    )}
+}
+export default App
